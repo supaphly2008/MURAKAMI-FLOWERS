a98a6303d5bc02512b6e175e382baeecd1a7cb88
Terry <liaoiyang@gmail.com>
2022-04-04 19:11:25 +0800

    add public sale page

---
+++ pages/publicSale.js
@@ -0,0 +1,13 @@
+const publicSale = () => {
+  return (
+    <div>
+      <header className="h-[68px] px-[20px] py-[15px] flex items-center justify-center text-[40px] font-[500] sm:text-[30px] relative">
+        <div>MURAKAMI.FLOWERS</div>
+        <div className="cursor-pointer h-[30px] absolute right-[20px] bg-white text-black rounded-full text-[18px] min-w-[80px] flex items-center justify-center ">Connect</div>
+      </header>
+      <div></div>
+    </div>
+  );
+};
+
+export default publicSale;
