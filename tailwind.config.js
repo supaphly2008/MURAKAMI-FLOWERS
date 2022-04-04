792ca8afe52ee9b6a7a85d1ff93e7f928d1cb087
Terry <liaoiyang@gmail.com>
2022-04-04 16:15:42 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "footer--gray": "rgba(255,255,255,0.8)",
+      black: "#000000",
+      white: "#ffffff",
+      transparent: "transparent",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
