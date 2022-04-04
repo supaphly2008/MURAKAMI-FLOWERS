35f536c1222965e2254afe1f37b7edef3a866419
Terry <liaoiyang@gmail.com>
2022-04-04 23:57:32 +0800

    add toast message

--- pages/publicSale.js
+++ pages/publicSale.js
@@ -1,7 +1,12 @@
 import { useAddress, useDisconnect, useMetamask, useNFTDrop, useEditionDrop } from "@thirdweb-dev/react";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const collectionAddress = "0xFB38ce2aF3857456E51Ab68C44A589Db44809f72";
+
+const ERROR_CODE = {
+  4001: "User denied transaction signature",
+};
 
 const PublicSale = () => {
   const address = useAddress();
@@ -18,18 +23,48 @@ const PublicSale = () => {
     const tokenId = 0;
     const quantity = 1;
     setLoading(true);
+    const notification = toast.loading("Minting NFT....", {
+      style: {
+        background: "white",
+        color: "green",
+        fontWeight: "bolder",
+        fontSize: "17px",
+        padding: "20px",
+      },
+    });
     try {
-      await editionDrop?.claimTo(address, tokenId, quantity);
-      console.log("🎉 NFT claimed successfully!");
+      const tx = await editionDrop?.claimTo(address, tokenId, quantity);
+      console.log("tx", tx);
+      toast.success(`You've successfully minted MURAKAMI FLOWERS!`, {
+        duration: 5000,
+        style: {
+          background: "white",
+          color: "green",
+          fontWeight: "bolder",
+          fontSize: "17px",
+          padding: "20px",
+        },
+      });
     } catch (err) {
       console.log("💩 Error claiming NFT: ", err);
+      toast.error(ERROR_CODE[err.code] || "Something went wrong!", {
+        style: {
+          background: "white",
+          color: "red",
+          fontWeight: "bolder",
+          fontSize: "17px",
+          padding: "20px",
+        },
+      });
     } finally {
       setLoading(false);
+      toast.dismiss(notification);
     }
   };
 
   return (
     <div>
+      <Toaster position="bottom-right" />
       <header className="h-[68px] px-[20px] py-[15px] flex sm:justify-start items-center justify-center text-[40px] font-[500] sm:text-[30px] relative">
         <div>MURAKAMI.FLOWERS</div>
         <div className="px-[20px] cursor-pointer h-[30px] absolute right-[20px] bg-white text-black rounded-full text-[18px] min-w-[80px] flex items-center justify-center" onClick={onConnectClick}>
@@ -52,11 +87,11 @@ const PublicSale = () => {
       </div>
       <div className="text-center text-[40px] font-[700] mt-[50px] sm:text-[30px]">
         <div>Mint Price</div>
-        <div>0.3 ETH / EACH</div>
-        <div>124 / 1000 Minted</div>
+        <div>0.108 ETH / EACH</div>
+        <div>641 / 1,000 Claimed</div>
       </div>
       <div className="flex justify-center mt-[30px]">
-        <button disabled={!address} className="px-[20px] disabled:bg-gray disabled:cursor-not-allowed font-[700] cursor-pointer h-[40px] bg-white text-black rounded-full text-[24px] min-w-[120px] flex items-center justify-center" onClick={mintNft}>
+        <button disabled={!address || loading} className="px-[20px] disabled:bg-gray disabled:cursor-not-allowed font-[700] cursor-pointer h-[40px] bg-white text-black rounded-full text-[24px] min-w-[120px] flex items-center justify-center" onClick={mintNft}>
           {!address ? "Connect to Mint" : "MINT"}
         </button>
       </div>
